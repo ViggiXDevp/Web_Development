7f52de465a1f608ff25657a37800e99d68891ec0
G V VIGHNESH REDDY <gvvighneshreddy8612@gmail.com>
2025-11-08 09:50:37 +0530

    JavaScript Conditionals, Operators

---
+++ LECTURE 56/index.js
@@ -0,0 +1,22 @@
+console.log("Hello from Lecture 56!");
+
+let age = 17;
+let grace = 2;
+
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age % grace);
+console.log(age ** grace);
+
+if((age + grace)> 18){
+    console.log("You can drive");
+} else{
+    console.log("You cannot drive");
+}
+
+a = 35;
+b = 10;
+let c = (a > b) ? (a-b) : (b-a);
+console.log(c);
