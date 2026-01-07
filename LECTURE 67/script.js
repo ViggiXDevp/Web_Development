70e2ac4684a08626287b400ca94f9c7917d78b0b
G V VIGHNESH REDDY <gvvighneshreddy8612@gmail.com>
2026-01-07 10:27:39 +0530

    DOM - Children, Parent & Sibling Nodes

---
+++ LECTURE 67/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+let cont = document.body.childNodes[1]
+cont.firstChild
+cont.childNodes[2]
+cont.firstElementChild
+cont.firstElementChild.style.backgroundColor = "aqua"
+cont.lastElementChild.parentElement
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
